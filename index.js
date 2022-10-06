9cd608f34fa2baf7bdb3856e0703257e21ac0c31
Xavier <sw.xavier@gmail.com>
2022-10-06 05:08:32 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// let name = 'Xavier';
+// let age = 33;
+// let isApproved = true;
+// let firstName = undefined;
+// let lastName = null;
+// let person = {
+//     name: 'Xavier',
+//     age: 33,
+// };
+
+// person.name = 'Xavi';
+// person['name'] = 'Mary';
+
+//Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith');
+greet('Mary', 'Jones');
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+for (let index = 0; index < 10; index++) {
+    console.log(square(index));
+}
+
+// let selectedColors = ['red', 'blue'];
+// console.log(selectedColors[0]);
